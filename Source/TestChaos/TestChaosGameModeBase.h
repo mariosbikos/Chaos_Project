75d9d4417964dfbda1ba3dcdec59cc6dd6fddee0
mariosbikos <mariosbikos@gmail.com>
2019-09-02 15:17:00 +0100

    Initial Commit

---
+++ Source/TestChaos/TestChaosGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestChaosGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCHAOS_API ATestChaosGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
